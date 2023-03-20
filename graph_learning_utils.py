1f2eac1e5b90db2b98afe676f5108ab52e9493e0
Akhil Jalan <akhiljalan@berkeley.edu>
2023-03-20 18:08:46 -0500

    code updates

---
+++ graph_learning_utils.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import scipy 
+
+def gen_sample_mat(ground_truth_mat):
+    '''
+    Generate a sample matrix from a ground truth matrix
+    inputs:
+        ground_truth_mat: A n by n matrix of probabilities
+    outputs:    
+        A: a n by n matrix whose entries are 1 
+        with probability ground_truth_mat[i, j] and 0 otherwise
+    '''
+    n = ground_truth_mat.shape[0]
+    A = np.zeros_like(ground_truth_mat)
+    for i in range(n):
+        for j in range(i + 1):
+            sample = np.random.binomial(1, ground_truth_mat[i, j])
+            A[i, j] = sample
+            A[j, i] = sample
+    return A
+
+def generate_synthetic_data(n, num_sample_mats):  
+    '''
+    n: number of rows and columns in the ground truth matrix
+    num_sample_mats: number of sample matrices to generate
+    '''
+    ground_truth_asymm = np.random.uniform(0, 0.5, (n, n))
+    ground_truth_mat = ground_truth_asymm + ground_truth_asymm.T
+
+    sample_mats = [gen_sample_mat(ground_truth_mat) for _ in range(num_sample_mats)]
+
+    scale = np.sum(sample_mats[0])
+    for i in range(len(sample_mats)):
+        if i > 0:
+            s = np.sum(sample_mats[i])
+            rescale = scale / s 
+            sample_mats[i] = rescale * sample_mats[i]
+    return sample_mats 
+
+def matrix_lin_combo(eta_arr, sample_mats): 
+    '''
+    eta_arr: array of weights for linear combination
+    sample_mats: array of sample matrices
+
+    returns: linear combination of the form
+    eta_0 * sample_mats[0] + sum_{i > 0} eta_i * (X_0 - X_i)
+    '''
+    X_0 = np.copy(eta_arr[0] * sample_mats[0])
+    for i in range(1, len(sample_mats)): 
+        X_i = eta_arr[i] * (X_0 - sample_mats[i])
+        X_0 += X_i
+    return X_0
+
+def get_delta(X_0): 
+    average_degree = np.mean(np.sum(X_0, axis=0))
+    return np.sqrt(average_degree)
