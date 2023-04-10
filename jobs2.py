74fbd7f3b9e50deeb825ec887a53f5fcb9606f5d
Akhil Jalan <akhiljalan@berkeley.edu>
2023-04-10 11:27:53 -0500

    run overnight tests

--- jobs2.py
+++ jobs2.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 import numpy as np
+import sys
+
 # import matplotlib.pyplot as plt
 # import seaborn as sns
 # import pandas as pd
@@ -15,6 +17,7 @@ import graph_learning_utils as gl
 # plt.rcParams['ytick.labelsize'] = 20.0
 # import pickle
 from itertools import product
+import pandas as pd
 
 def get_cur_time_str(): 
     now = datetime.now() # current date and time
@@ -38,23 +41,30 @@ def gen_sample_data(base_ppi_mat, m=5, occlusion_rate=0.01, delta_scaling = 0.1)
 def print_ln(): 
     print('--------------------------\n')
 
-def main(): 
-    print_ln()
+def main(out_path): 
+    out_df = pd.DataFrame()
     cur_time = get_cur_time_str()
+
     print('BEGIN TEST at time {}'.format(cur_time))
     human_ppi_mat = load_human_ppi()
     num_eigs_to_include = 10
 
-    m_range = [20, 25, 30]
+    m_range = [5, 10, 15, 20, 30]
     delta_scale_range = [0.0, 0.1] #exclude 0.5
-    occlusion_levels = [0.0, 0.01, 0.1, 0.5]
+    occlusion_levels = [0.0, 0.1, 0.5]
     # k_levels = [10, 20, 30]
     num_eigs_to_test = 20
+
     param_sets = product(m_range, delta_scale_range, occlusion_levels)
     for m_val, delta_val, occ_val in param_sets: 
+        cur_data = {
+            'm': m_val, 
+            'delta_scaling': delta_val, 
+            'occlusion_level': occ_val
+        }
         print_ln()
-        cur_time = get_cur_time_str()
-        print('BEGIN TEST at time {}'.format(cur_time))
+        begin_time = get_cur_time_str()
+        print('BEGIN TEST at time {}'.format(begin_time))
         print('Running test with m={}, delta_scaling={}, occlusion level={}, k={}'.format(m_val, 
                                                                                           delta_val, occ_val,num_eigs_to_include))
 
@@ -78,17 +88,29 @@ def main():
             bounds=[(0, 1) for _ in range(len(sample_mats))],
             options=bfgs_options
         )
-        print(result)
+        # print(result)
         validation_diff_svals, true_diff_svals, delta_est, true_delta = gl.result_diff(result, sample_mats, 
             validation_mat, human_ppi_mat, num_eigs_included=num_eigs_to_test)
-        print('L2 diff of validation and ground truth {}'.format(true_delta))
-        print('Top {} validation diff singular values {}'.format(num_eigs_to_test, validation_diff_svals))
-        print('Top {} v true diff singular values {}'.format(num_eigs_to_test, true_diff_svals))
+        # print('L2 diff of validation and ground truth {}'.format(true_delta))
+        # print('Top {} validation diff singular values {}'.format(num_eigs_to_test, validation_diff_svals))
+        # print('Top {} v true diff singular values {}'.format(num_eigs_to_test, true_diff_svals))
 
-        cur_time = get_cur_time_str()
-        print('END TEST at time {}'.format(cur_time))
+        end_time = get_cur_time_str()
+        print('END TEST at time {}'.format(end_time))
         print_ln()
+        time_diff = (end_time - begin_time).total_seconds()
 
+        cur_data['l2_diff_val_gt'] = true_delta
+        cur_data['validation_diff_svals'] = validation_diff_svals
+        cur_data['true_diff_svals'] = true_diff_svals
+        cur_data['time_diff'] = time_diff
+        cur_data.update(result)
+
+        out_df = out_df.append(cur_data, ignore_index=True)
+        out_df.to_csv(out_path)
 
 if __name__ == '__main__': 
-    main()
+    now = datetime.now()
+    out_path = 'logs/job_{}.csv'.format(now.strftime("%m_%d_%Y_%H_%M_%S"))
+    print('Writing to {}'.format(out_path))
+    main(out_path=out_path)
